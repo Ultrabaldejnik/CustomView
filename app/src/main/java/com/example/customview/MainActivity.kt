d3d5d9e5add688534bf2c9997f0c7c675cc738f1
Sergey <svakarin@inbox.ru>
2023-11-04 23:45:36 +0300

    Initial commit

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
     }
 }
